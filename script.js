e29a1ed8a36763e51c7a7fdf034a2a5514a39d48
richterndzh <richterndzh@gmail.com>
2022-01-28 19:47:45 +0300

    Массив

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let title ="";
-let screens ="";
-let screenPrice ="";
-let rollback ="";
-let fullPrice ="";
-let adaptive ="";
+let title ="JavaScript";
+let screens ="Простые, Сложные, Интерактивные";
+let screenPrice ="17";
+let rollback ="29";
+let fullPrice ="40000";
+let adaptive ="true";
 
-alert("Hello,World");
-console.log("Hello console!");
+console.log(typeof title);
+console.log(screens.length);
+console.log(screens.toLowerCase().split(","));
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
+console.log("Стоимость разработки сайтов" + " " + rollback + " " + "долларов");
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(fullPrice * (rollback / 100));
+
+// alert("Hello,World");
+// console.log("Hello console!");
