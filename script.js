87574420812209efdff5fbf1e2aae8e89105ea6f
richterndzh <richterndzh@gmail.com>
2022-01-29 15:49:55 +0300

    Added comments

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ let rollback = 29;
 let fullPrice = 40000;
 let adaptive = true;
 
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 console.log(typeof title);
-console.log(screens.length);
-console.log(screens.toLowerCase().split(","));
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
-console.log("Стоимость разработки сайтов" + " " + rollback + " " + "долларов");
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
+// Вывести в консоль длину строки screens
+console.log(screens.length);
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLowerCase().split(","));
+/** Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и т.д*/
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
+console.log("Стоимость разработки сайта" + " " + rollback + " " + "долларов");
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
 console.log(fullPrice * (rollback / 100));
 
 // alert("Hello,World");
